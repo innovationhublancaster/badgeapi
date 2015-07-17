4230c25a1880ab2e9fb2897428a51c06ee707b53
Tom Skarbek Wazynski <t.skarbek-wazynski@lancaster.ac.uk>
2015-07-17 16:43:37 +0100

    Gem is handling recipients without badges currently. Testing fine. Removed some old fluff now longer needed against collection.rb.

---
+++ lib/badgeapi/recipient.rb
@@ -0,0 +1,18 @@
+#lib/badgeapi/collection.rb
+
+module Badgeapi
+	class Recipient < BadgeapiObject
+
+		attr_reader :total_score, :badges_total, :bronze_count, :silver_count, :gold_count, :platinum_count
+
+		class << self
+
+			def find params = {}
+				request "get", "#{Badgeapi.api_url}/#{collection_path}", params
+			end
+
+			undef_method :all, :create, :update, :destroy
+		end
+
+	end
+end
